01c6c991dff1e3c41f374072c940df18ee81ea22
mfjaconis <matheusjaconis@gmail.com>
2024-02-22 19:14:56 -0300

    iniciando a HOME do projeto

---
+++ src/assets/components/Home/index.jsx
@@ -0,0 +1,21 @@
+
+
+export function Home(){
+
+    return(
+        <section>
+            <img />
+            <h1>Kaffi Agency</h1>
+            <button>Contrate nossos serviços</button>
+            <button>Quem somos nós</button>
+            <button>WhatsApp</button>
+        </section>
+    );
+}
+
+
+
+
+
+
+
