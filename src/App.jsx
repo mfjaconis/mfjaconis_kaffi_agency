f8ee30266af29e5a9d74fc59ee2bc5dc5d153206
mfjaconis <matheusjaconis@gmail.com>
2024-02-28 16:24:33 -0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,4 @@
-import Routes from "./Routes.jsx";
-
-
-import { register } from "swiper/element/bundle";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-
-register();
+import { Routes } from "./Routes.jsx";
 
 function App() {
   return <Routes />;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
-import "./index.css";
-import { BrowserRouter  } from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
 
 import { register } from "swiper/element/bundle";
-
-register();
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-
-
+register();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <BrowserRouter>
+    <BrowserRouter basename="/mfjaconis_kaffi_agency">
       <App />
     </BrowserRouter>
   </React.StrictMode>
