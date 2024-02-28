61c9d8901c610323104fb3a092361b340a67d875
mfjaconis <matheusjaconis@gmail.com>
2024-02-28 16:06:41 -0300

    deploy

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,15 @@
 import { Route, BrowserRouter } from "react-router-dom";
 
-import Home from "./Home";
-import Sobre from "./Sobre";
+import { Home } from "./assets/components/Home";
+import { Sobre } from "./assets/components/Sobre";
 
 const Routes = () => {
-   return(
-       <BrowserRouter>
-           <Route component = { Home }  path="/" exact />
-           <Route component = { Sobre }  path="/sobre" />
-       </BrowserRouter>
-   )
-}
+  return (
+    <BrowserRouter>
+      <Route component={Home} path="/" exact />
+      <Route component={Sobre} path="/sobre" />
+    </BrowserRouter>
+  );
+};
 
 export default Routes;
